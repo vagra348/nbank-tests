25ca62849653dbd3b805e20d37ed45125342f9e8
vagra348 <vagra348@gmail.com>
2026-01-05 05:18:24 +0300

    fix: flaky tests fixed (read flaky-failures.txt)

--- src/main/java/ui/pages/UserDashboard.java
+++ src/main/java/ui/pages/UserDashboard.java
@@ -1,11 +1,16 @@
 package ui.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
+import common.utils.RetryUtils;
 import lombok.Getter;
 import ui.elements.WelcomeTitle;
 
+import java.util.Objects;
+
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.switchTo;
 
 @Getter
 public class UserDashboard extends BasePage<UserDashboard> {
@@ -17,13 +22,30 @@ public class UserDashboard extends BasePage<UserDashboard> {
     private SelenideElement transferMoneyBtn = $(Selectors.byText("\uD83D\uDD04 Make a Transfer"));
     private SelenideElement userNameProfile = $(Selectors.byXpath("//*[@class='user-name']"));
 
-    public UserDashboard createNewAccount() {
-        createAccBtn.click();
+    public UserDashboard createNewAccount(String alertName) {
+        RetryUtils.retryVoidWithCheck(
+                () -> {
+                    createAccBtn.shouldBe(Condition.visible);
+                    createAccBtn.click();
+                },
+                () -> switchTo().alert().getText().contains(alertName),
+                3,
+                1000
+        );
+
         return this;
     }
 
     public WelcomeTitle getWelcomeTitle() {
         return generatePageElement(welcomeText, WelcomeTitle::new);
+    }
+    public String getUserNameInWelcomeText() {
+        return RetryUtils.retry(
+                () -> getWelcomeTitle().getUserName(),
+                result -> !Objects.equals(getWelcomeTitle().getUserName(), "noname"),
+                7,
+                2000
+        );
     }
 
     @Override
