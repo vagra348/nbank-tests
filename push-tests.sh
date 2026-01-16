bec97c2f80fd0ca23d816e2a05d516bcbffb4475
vagra348 <vagra348@gmail.com>
2026-01-16 17:50:16 +0300

    add: push to docker attempt

--- push-tests.sh
+++ push-tests.sh
@@ -2,13 +2,19 @@
 
 IMAGE_NAME=nbank-tests
 DOCKERHUB_USERNAME=vagra348
-TAG=latest
+TAG=${1:-latest}
 
 if [ -z "$DOCKERHUB_TOKEN" ]; then
     echo "ERROR: Please set DOCKERHUB_TOKEN environment"
     echo "Example: export DOCKERHUB_TOKEN='<your-token>'"
     exit 1
 fi
+
+echo ">>> Building project..."
+./mvnw clean package -DskipTests
+
+echo ">>> Building Docker image..."
+docker build -t $IMAGE_NAME:$TAG
 
 echo ">>> Login to Docker Hub with token"
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
